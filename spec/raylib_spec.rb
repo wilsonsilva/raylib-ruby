8134e2f6b846091f874e56f32aec107c5a99f5f8
Wilson Silva <wilson.dsigns@gmail.com>
2023-09-22 17:56:54 +0700

    Initial commit

---
+++ spec/raylib_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Raylib do
+  it "has a version number" do
+    expect(Raylib::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
