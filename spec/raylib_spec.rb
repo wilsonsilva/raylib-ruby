e0d59400b0c1ebf96be0cbd192d4b68f61db1707
Wilson Silva <wilson.dsigns@gmail.com>
2023-10-18 12:34:44 +0700

    Remove an unnecessary spec

--- spec/raylib_spec.rb
+++ spec/raylib_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Raylib do
   it "has a version number" do
     expect(Raylib::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
